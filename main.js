b4a5a75387577c053bd7ec63492d3c17d0823989
dorseyj <36482026+dorseyj@users.noreply.github.com>
2022-03-30 17:56:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ loader.load(
 
         var canvas = document.getElementById('canvas');
         canvas.style.visibility = "hidden";
+        var errorScreen = document.getElementById('error-screen');
         errorScreen.style.visibility = "hidden";
         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
         if ((xhr.loaded / xhr.total * 100 ) === 100)
